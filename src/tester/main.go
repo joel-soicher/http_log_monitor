fce13f7497d774dd5973beac65fb8b76c72e0dcc
Joel Soicher <joel.soicher@adyoulike.com>
2019-04-15 11:40:57 +0200

    Modify tester to check triggered alerts

--- src/tester/main.go
+++ src/tester/main.go
@@ -14,10 +14,20 @@ func main() {
 	defer f.Close()
 
 	for {
-		if _, err := f.Write([]byte("127.0.0.1 - james [09/May/2018:16:00:39 +0000] \"GET /report HTTP/1.0\" 200 123\n")); err != nil {
+		if _, err := f.Write([]byte("127.0.0.1 - james [09/May/2018:16:00:39 +0000] \"GET /report/t HTTP/1.0\" 200 123\n")); err != nil {
 			log.Fatal(err)
 		}
 
-		time.Sleep(8 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
+
+		if _, err := f.Write([]byte("127.0.0.1 - jill [09/May/2018:16:00:41 +0000] \"GET /api/user HTTP/1.0\" 200 234\n")); err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(10 * time.Millisecond)
+		if _, err := f.Write([]byte("127.0.0.1 - frank [09/May/2018:16:00:42 +0000] \"POST /api/user HTTP/1.0\" 200 34\n")); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(10 * time.Millisecond)
 	}
 }
